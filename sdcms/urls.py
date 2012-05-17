eadca6605480e16befeaf547339d1049bec30cd7
Walt S <tufelkinder@yahoo.com(mp)>
2012-05-16 23:47:49 -0400

    fixed non-recognition of hyphens in urls

--- sdcms/urls.py
+++ sdcms/urls.py
@@ -7,13 +7,16 @@ admin.autodiscover()
 urlpatterns = patterns('sdcms.manager.views',
     # Examples:
     url(r'^$', 'index', name='index'),
-    url(r'^go/(?P<page_name>\w+)/', 'go'),
+    url(r'^go/(?P<page_name>[-\w]+)/', 'go'),
     url(r'^gallery/(?P<gal_id>\w+)/', 'gallery'),
     url(r'^gallery/', 'gallery'),
     url(r'^blog/(?P<blog_id>\w+)/', 'blog'),
     url(r'^blog/', 'blog'),
     url(r'^contact/', 'contact'),
     url(r'^g_path/(?P<g_id>\d+)/','g_path'),
+
+# this entry should be last
+    url(r'^(?P<page_name>[-\w]+)$', 'go', name='go'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
