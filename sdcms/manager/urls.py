312d55adad3e5e1c6c28cc650ce309e738c9eff7
Walt S <tufelkinder@yahoo.com>
2013-04-16 10:53:17 -0400

    disabled catch-all url

--- sdcms/manager/urls.py
+++ sdcms/manager/urls.py
@@ -15,12 +15,13 @@ urlpatterns = patterns('manager.views',
     url(r'^contact/', 'contact'),
     url(r'^g_path/(?P<g_id>\d+)/','g_path'),
 
-# this entry should be last
-    url(r'^(?P<page_name>[-\w]+)$', 'go', name='go'),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # this entry should be last
+#    url(r'^(?P<page_name>[-\w]+)$', 'go', name='go'),
+
 )
